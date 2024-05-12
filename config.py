220c453c4b681cd0e395d44e5de0ab137be169f4
PandazNetwork <156095336+PandazNetwork@users.noreply.github.com>
2024-05-13 01:18:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,5 +10,5 @@ ADMINS = environ.get("ADMINS", "1111214141,1646672520").split(",")
 ADMIN_IDS = [int(admin.strip()) for admin in ADMINS]
 
 #Database
-DB_URI = environ.get('DB_URI', ')
+DB_URI = environ.get('DB_URI', '')
 DB_NAME = environ.get('DB_NAME', 'Pastes')
